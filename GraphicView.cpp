a4228cd7eccde3872e294fb227fc3186bda4ac7f
先升 呆 <ak6682928@hotmail.com>
2019-12-22 11:00:36 +0800

    添加项目文件。

---
+++ GraphicView.cpp
@@ -0,0 +1,105 @@
+﻿
+// GraphicView.cpp: CGraphicView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "Graphic.h"
+#endif
+
+#include "GraphicDoc.h"
+#include "GraphicView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CGraphicView
+
+IMPLEMENT_DYNCREATE(CGraphicView, CView)
+
+BEGIN_MESSAGE_MAP(CGraphicView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CGraphicView 构造/析构
+
+CGraphicView::CGraphicView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CGraphicView::~CGraphicView()
+{
+}
+
+BOOL CGraphicView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CGraphicView 绘图
+
+void CGraphicView::OnDraw(CDC* /*pDC*/)
+{
+	CGraphicDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CGraphicView 打印
+
+BOOL CGraphicView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CGraphicView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CGraphicView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CGraphicView 诊断
+
+#ifdef _DEBUG
+void CGraphicView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CGraphicView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CGraphicDoc* CGraphicView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CGraphicDoc)));
+	return (CGraphicDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CGraphicView 消息处理程序
---
+++ GraphicView.h
@@ -0,0 +1,49 @@
+﻿
+// GraphicView.h: CGraphicView 类的接口
+//
+
+#pragma once
+
+
+class CGraphicView : public CView
+{
+protected: // 仅从序列化创建
+	CGraphicView() noexcept;
+	DECLARE_DYNCREATE(CGraphicView)
+
+// 特性
+public:
+	CGraphicDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CGraphicView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // GraphicView.cpp 中的调试版本
+inline CGraphicDoc* CGraphicView::GetDocument() const
+   { return reinterpret_cast<CGraphicDoc*>(m_pDocument); }
+#endif
+
