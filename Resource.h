82e6dcd5d6ce9cf22caafedab6b5c4611bbc6558
先升 呆 <ak6682928@hotmail.com>
2019-12-28 09:36:56 +0800

    11章位图也添加进去了

--- Resource.h
+++ Resource.h
@@ -10,6 +10,7 @@
 #define IDB_PNG1                        313
 #define IDB_PNG2                        315
 #define IDB_PNG3                        316
+#define IDB_BITMAP1                     318
 #define IDC_LINE_WIDTH                  1000
 #define IDC_RADIO1                      1001
 #define IDC_RADIO2                      1002
@@ -35,7 +36,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        317
+#define _APS_NEXT_RESOURCE_VALUE        321
 #define _APS_NEXT_COMMAND_VALUE         32784
 #define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           310
